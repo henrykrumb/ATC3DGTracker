5e92f1aa91ecbe6f09cbd0c0c0dc80c3713db6ba
Henry Krumb <mail@henrykrumb.de>
2021-10-04 12:21:40 +0200

    improve whitespace, brackets

--- src/atclib.cpp
+++ src/atclib.cpp
@@ -40,61 +40,64 @@ const int DELAY = 500;
 // Conversions
 const double WTF = 1.0/32768; // word to float
 const double ANGK = 180 * WTF;
-const double INCH_TO_METER  = 25.4/1000.0;
+const double INCH_TO_METER  = 25.4 / 1000.0;
 const double POSK36 = 36 * WTF * INCH_TO_METER;
 const double POSK72 = 72 * WTF * INCH_TO_METER;
+
 
 void PointATC3DG::write(unsigned int bytes) {
     ret = usb_bulk_write( handle, BIRD_EP_OUT, dataout, bytes, DELAY);
 }
 
+
 void PointATC3DG::read(unsigned int bytes) {
-    do {                                                                    
+    do {
         ret = usb_bulk_read( handle, BIRD_EP_IN, datain, bytes, DELAY);      
-    } while( ret == 0);
+    } while (ret == 0);
 }
 
+
 PointATC3DG::PointATC3DG(unsigned int productId, unsigned int vendorId)
-: vendorId(vendorId)
-, productId(productId)
-, isOk(true)
+	: vendorId(vendorId),
+	productId(productId),
+	isOk(true)
 {
     usb_init();
 
     dev = find_device( vendorId, productId);
-    if( !dev ) {
+    if(!dev) {
         error( 1, "finding device on USB bus. Turn it on?");
         return;
     }
 
     handle = usb_open( dev);
-    if( !handle ) {
-        error( 2, "claiming USB device -> %s", usb_strerror());
+    if (!handle) {
+        error(2, "claiming USB device -> %s", usb_strerror());
         return;
     }
 
     ret = usb_set_configuration( handle, 1);
-    if( ret < 0 ) {
-        error( ret, "setting configuration on USB device."
+    if (ret < 0) {
+        error(ret, "setting configuration on USB device."
                     " Check device permissions? -> %s", usb_strerror());
         return;
     }
     ret = usb_claim_interface( handle, 0);
-    if( ret < 0 ) {
-        error( ret, "claiming USB interface on device -> %s", usb_strerror());
+    if (ret < 0) {
+        error(ret, "claiming USB interface on device -> %s", usb_strerror());
         return;
     }
     ret = usb_set_altinterface( handle, 0);
-    if( ret < 0 ) {
-        error( ret, "setting alternate interface -> %s", usb_strerror());
+    if (ret < 0) {
+        error(ret, "setting alternate interface -> %s", usb_strerror());
         return;
     }
     ret = usb_clear_halt( handle, BIRD_EP_IN);
-    if( ret < 0 ) {
-        error( ret, "clearing halt on EP_IN -> %s", usb_strerror());
+    if (ret < 0) {
+        error(ret, "clearing halt on EP_IN -> %s", usb_strerror());
         return;
     }
-    ret = usb_bulk_read( handle, BIRD_EP_IN, datain, 32, DELAY);
+    ret = usb_bulk_read(handle, BIRD_EP_IN, datain, 32, DELAY);
 
 //    check_bird_errors();
 
@@ -102,8 +105,8 @@ PointATC3DG::PointATC3DG(unsigned int productId, unsigned int vendorId)
     dataout[1] = FBB_AUTO_CONFIGURATION;
     dataout[2] = 0x01;
     write(3);
-    if( ret < 0 ) {
-        error( ret, "sending FBB_AUTO_CONFIGURATION -> %s", usb_strerror());
+    if (ret < 0) {
+        error(ret, "sending FBB_AUTO_CONFIGURATION -> %s", usb_strerror());
         return;
     }
     usleep( 600000); // delay 600 ms after auto-configuration
@@ -112,35 +115,40 @@ PointATC3DG::PointATC3DG(unsigned int productId, unsigned int vendorId)
     dataout[1] = BIRD_POSITION_SCALING;
     write(2);
     read(2);
-    if( ret < 0 ) {
-        error( ret, "querying scaling factor -> %s", usb_strerror());
+    if (ret < 0) {
+        error(ret, "querying scaling factor -> %s", usb_strerror());
         return;
     }
     posk = POSK36;
-    if( datain[0] == 1 )
+    if (datain[0] == 1) {
         posk = POSK72;
+	}
 
     check_bird_errors();
 }
 
+
 PointATC3DG::~PointATC3DG()
 {
-    if( dev && handle ) {
+    if (dev && handle) {
         dataout[0] = SLEEP;
         write(1);
-        usb_close( handle);
+        usb_close(handle);
     }
 }
+
 
 bool PointATC3DG::operator!() const
 {
     return !isOk;
 }
 
+
 bool PointATC3DG::ok() const
 {
     return isOk;
 }
+
 
 int PointATC3DG::setSuddenOutputChangeLock( int iSensorId )
 {
@@ -153,6 +161,7 @@ int PointATC3DG::setSuddenOutputChangeLock( int iSensorId )
     return check_bird_errors();
 }
 
+
 int PointATC3DG::setSensorQuaternion( int iSensorId )
 {
     dataout[0] = 0xf1 + iSensorId;
@@ -162,7 +171,8 @@ int PointATC3DG::setSensorQuaternion( int iSensorId )
     return check_bird_errors();
 }
 
-int PointATC3DG::setSensorRotMat( int iSensorId )
+
+int PointATC3DG::setSensorRotMat(int iSensorId)
 {
     dataout[0] = 0xf1 + iSensorId;
     dataout[1] = POS_MAT;
@@ -171,17 +181,20 @@ int PointATC3DG::setSensorRotMat( int iSensorId )
     return check_bird_errors();
 }
 
-int PointATC3DG::setSensorTopHemisphere( int iSensorId )
+
+int PointATC3DG::setSensorTopHemisphere(int iSensorId)
 {
     return check_bird_errors();
 }
 
-int PointATC3DG::setSensorHemisphere( int iSensorId, char cSphereId )
+
+int PointATC3DG::setSensorHemisphere(int iSensorId, char cSphereId)
 {
     return check_bird_errors();
 }
 
-int PointATC3DG::setMeasurementRate( double dRate )
+
+int PointATC3DG::setMeasurementRate(double dRate)
 {
     short sRate = (short) (dRate * 256);
     dataout[0] = CHANGE_VALUE;
@@ -193,21 +206,23 @@ int PointATC3DG::setMeasurementRate( double dRate )
     return check_bird_errors();
 }
 
-int PointATC3DG::getNumberOfSensors( void )
 
+int PointATC3DG::getNumberOfSensors(void)
 {
     int nSensors = 0;
-    for( char i = 0x00 ; i < 0x04 ; ++i ) {
+    for (char i = 0x00 ; i < 0x04 ; ++i) {
         dataout[0] = 0xf1 + i;
         dataout[1] = EXAMINE_VALUE;
         dataout[2] = SENSOR_SERIAL_NUMBER;
         write(3);
         read(2);
-        if( ret < 0 ) {
+        if (ret < 0) {
             error( ret, "getting serial number for sensor %d -> %s",
                         i, usb_strerror());
         }
-        else if( datain[0] || datain[1] ) ++nSensors;
+        else if (datain[0] || datain[1]) {
+			++nSensors;
+		}
     }
     return nSensors;
 }
@@ -224,30 +239,30 @@ int PointATC3DG::getCoordinatesAngles( int iSensorId,
 
     read(12);
 
-    if( ret == 12 ) {
+    if (ret == 12) {
         nX = ( (datain[1] << 7) | (datain[0] & 0x7f) ) << 2;
         nY = ( (datain[3] << 7) | (datain[2] & 0x7f) ) << 2;
         nZ = ( (datain[5] << 7) | (datain[4] & 0x7f) ) << 2;
-    
+
         nAzimuth = ( (datain[7] << 7) | (datain[6] & 0x7f) ) << 2;
         nElevation = ( (datain[9] << 7) | (datain[8] & 0x7f) ) << 2;
         nRoll = ( (datain[11] << 7) | (datain[10] & 0x7f) ) << 2;
-    
+
         dX = nX * posk;
         dY = nY * posk;
         dZ = nZ * posk;
-    
+
         dAzimuth = nAzimuth * ANGK;
         dElevation = nElevation * ANGK;
         dRoll = nRoll * ANGK;
-    
+
         return check_bird_errors();
     }
-    error( ret, "reading point data -> %s", usb_strerror());
+    error(ret, "reading point data -> %s", usb_strerror());
     return ret;
 }
 
-int PointATC3DG::getCoordinatesMatrix( int iSensorId,
+int PointATC3DG::getCoordinatesMatrix(int iSensorId,
                      double& dX, double& dY, double& dZ,
                      double* pMat )
 {
@@ -260,13 +275,13 @@ int PointATC3DG::getCoordinatesMatrix( int iSensorId,
 
     read(24);
 
-    if( ret == 24 ) {
-        nX = ( (datain[1] << 7) | (datain[0] & 0x7f) ) << 2;
-        nY = ( (datain[3] << 7) | (datain[2] & 0x7f) ) << 2;
-        nZ = ( (datain[5] << 7) | (datain[4] & 0x7f) ) << 2;
+    if (ret == 24 ) {
+        nX = ((datain[1] << 7) | (datain[0] & 0x7f)) << 2;
+        nY = ((datain[3] << 7) | (datain[2] & 0x7f)) << 2;
+        nZ = ((datain[5] << 7) | (datain[4] & 0x7f)) << 2;
     
         short *dataptr = sMat;
-        for( int i = 7 ; i < 24 ; i += 2, ++dataptr ) {
+        for (int i = 7; i < 24 ; i += 2, ++dataptr) {
             *dataptr = ( (datain[i] << 7) | (datain[i-1] & 0x7f) ) << 2;
         }
     
@@ -274,12 +289,13 @@ int PointATC3DG::getCoordinatesMatrix( int iSensorId,
         dY = nY * posk;
         dZ = nZ * posk;
     
-        for( int i = 0 ; i < 9 ; ++i )
+        for (int i = 0 ; i < 9 ; ++i) {
             pMat[i] = sMat[i] * WTF;
+		}
     
         return check_bird_errors();
     }
-    error( ret, "reading point data -> %s", usb_strerror());
+    error(ret, "reading point data -> %s", usb_strerror());
     return ret;
 }
 
@@ -296,13 +312,13 @@ int PointATC3DG::getCoordinatesQuaternion( int iSensorId,
     
     read(14);
     
-    if( ret == 14 ) {
+    if (ret == 14) {
         nX = ( (datain[1] << 7) | (datain[0] & 0x7f) ) << 2;
         nY = ( (datain[3] << 7) | (datain[2] & 0x7f) ) << 2;
         nZ = ( (datain[5] << 7) | (datain[4] & 0x7f) ) << 2;
         
         short *dataptr = q;
-        for( int i = 7 ; i < 14 ; i += 2, ++dataptr ) {
+        for (int i = 7 ; i < 14 ; i += 2, ++dataptr ) {
             *dataptr = ( (datain[i] << 7) | (datain[i-1] & 0x7f) ) << 2;
         }
         
@@ -310,12 +326,13 @@ int PointATC3DG::getCoordinatesQuaternion( int iSensorId,
         dY = nY * posk;
         dZ = nZ * posk;
         
-        for( int i = 0 ; i < 4 ; ++i )
+        for (int i = 0 ; i < 4 ; ++i) {
             quat[i] = q[i] * WTF;
+		}
         
         return check_bird_errors();
     }
-    error( ret, "reading point data -> %s", usb_strerror() );
+    error(ret, "reading point data -> %s", usb_strerror());
     return ret;
 }
 
@@ -325,10 +342,12 @@ bool PointATC3DG::transmitterAttached()
     dataout[1] = TRANSMITTER_SERIAL_NUMBER;
     write(2);
     read(2);
-    if( ret < 0 ) {
-        error( ret, "getting serial number for transmitter -> %s", usb_strerror());
+    if (ret < 0) {
+        error(ret, "getting serial number for transmitter -> %s", usb_strerror());
     }
-    else if( datain[0] || datain[1] )   return true;
+    else if (datain[0] || datain[1]) {
+		return true;
+	}
     return false;
 }
 
@@ -339,11 +358,13 @@ bool PointATC3DG::sensorAttached(const int& iSensorId)
     dataout[2] = SENSOR_SERIAL_NUMBER;
     write(3);
     read(2);
-    if( ret < 0 ) {
-        error( ret, "getting serial number for sensor %d -> %s",
+    if (ret < 0) {
+        error(ret, "getting serial number for sensor %d -> %s",
                     iSensorId, usb_strerror());
     }
-    else if( datain[0] || datain[1] )   return true;
+    else if (datain[0] || datain[1]) {
+		return true;
+	}
     return false;
 }
 
@@ -355,9 +376,9 @@ struct usb_device* PointATC3DG::find_device( unsigned short iVendorId, unsigned 
     usb_find_busses();
     usb_find_devices();
 
-    for( bus = usb_busses ; bus ; bus = bus->next ) {
-        for( dev = bus->devices ; dev ; dev = dev->next ) {
-            if( dev->descriptor.idVendor == iVendorId &&
+    for (bus = usb_busses; bus; bus = bus->next) {
+        for (dev = bus->devices ; dev ; dev = dev->next ) {
+            if (dev->descriptor.idVendor == iVendorId &&
                 dev->descriptor.idProduct == iProductId ) {
                 return dev;
             }
@@ -374,31 +395,77 @@ int PointATC3DG::check_bird_errors( void )
     write(2);
     read(1);
 
-    if( datain[0] == 0 ) return 0;
+    if (datain[0] == 0) {
+		return 0;
+	}
 
-    switch( datain[0] ) {
-        case 1:     fprintf( stderr, "FATAL(1): System Ram Failure"); fatal = true; break;
-        case 2:     fprintf( stderr, "FATAL(2): Non-Volatile Storage Write Failure"); fatal = true; break;
-        case 3:     fprintf( stderr, "WARNING(3): PCB Configuration Data Corrupt"); break;
-        case 4:     fprintf( stderr, "WARNING(4): Bird Transmitter Calibration Data Corrupt or Not Connected"); break;
-        case 5:     fprintf( stderr, "WARNING(5): Bird Sensor Calibration Data Corrupt or Not Connected"); break;
-        case 6:     fprintf( stderr, "WARNING(6): Invalid RS232 Command"); break;
-        case 7:     fprintf( stderr, "WARNING(7): Not an FBB Master"); break;
-        case 8:     fprintf( stderr, "WARNING(8): No Birds Accessible in Device List"); break;
-        case 9:     fprintf( stderr, "WARNING(9): Bird is Not Initialized"); break;
-        case 10:    fprintf( stderr, "WARNING(10): FBB Serial Port Receive Error - Intra Bird Bus"); break;
-        case 11:    fprintf( stderr, "WARNING(11): RS232 Serial Port Receive Error"); break;
-        case 12:    fprintf( stderr, "WARNING(12): FBB Serial Port Receive Error"); break;
-        case 13:    fprintf( stderr, "WARNING(13): No FBB Command Response"); break;
-        case 14:    fprintf( stderr, "WARNING(14): Invalid FBB Host Command"); break;
-        case 15:    fprintf( stderr, "FATAL(15): FBB Run Time Error"); fatal = true; break;
-        case 16:    fprintf( stderr, "FATAL(16): Invalid CPU Speed"); fatal = true; break;
-        case 17:    fprintf( stderr, "WARNING(17): No FBB Data"); break;
-        case 18:    fprintf( stderr, "WARNING(18): Illegal Baud Rate"); break;
-        case 19:    fprintf( stderr, "WARNING(19): Slave Acknowledge Error"); break;
+    switch (datain[0]) {
+        case 1:
+			fprintf( stderr, "FATAL(1): System Ram Failure");
+			fatal = true;
+			break;
+        case 2:
+			fprintf( stderr, "FATAL(2): Non-Volatile Storage Write Failure");
+			fatal = true;
+			break;
+        case 3:
+			fprintf( stderr, "WARNING(3): PCB Configuration Data Corrupt");
+			break;
+        case 4:
+			fprintf( stderr, "WARNING(4): Bird Transmitter Calibration Data Corrupt or Not Connected");
+			break;
+        case 5:
+			fprintf( stderr, "WARNING(5): Bird Sensor Calibration Data Corrupt or Not Connected");
+			break;
+        case 6:
+			fprintf( stderr, "WARNING(6): Invalid RS232 Command");
+			break;
+        case 7:
+			fprintf( stderr, "WARNING(7): Not an FBB Master");
+			break;
+		case 8:
+			fprintf( stderr, "WARNING(8): No Birds Accessible in Device List");
+			break;
+        case 9:
+			fprintf( stderr, "WARNING(9): Bird is Not Initialized");
+			break;
+        case 10:
+			fprintf( stderr, "WARNING(10): FBB Serial Port Receive Error - Intra Bird Bus");
+			break;
+        case 11:
+			fprintf( stderr, "WARNING(11): RS232 Serial Port Receive Error");
+			break;
+        case 12:
+			fprintf( stderr, "WARNING(12): FBB Serial Port Receive Error");
+			break;
+        case 13:
+			fprintf( stderr, "WARNING(13): No FBB Command Response");
+			break;
+        case 14:
+			fprintf( stderr, "WARNING(14): Invalid FBB Host Command");
+			break;
+        case 15:
+			fprintf( stderr, "FATAL(15): FBB Run Time Error");
+			fatal = true;
+			break;
+        case 16:
+			fprintf( stderr, "FATAL(16): Invalid CPU Speed");
+			fatal = true;
+			break;
+        case 17:
+			fprintf( stderr, "WARNING(17): No FBB Data");
+			break;
+        case 18:
+			fprintf( stderr, "WARNING(18): Illegal Baud Rate");
+			break;
+        case 19:
+			fprintf( stderr, "WARNING(19): Slave Acknowledge Error");
+			break;
         case 20: case 21: case 22: case 23:
         case 24: case 25: case 26: case 27:
-            fprintf( stderr, "FATAL(%d): Intel 80186 CPU Errors", datain[0]); fatal = true; break;
+			std::cerr << "FATAL(" << datain[0] << "): Intel 80186 CPU Errors";
+			fatal = true;
+			break;
         case 28:    fprintf( stderr, "WARNING(28): CRT Synchronization"); break;
         case 29:    fprintf( stderr, "WARNING(29): Transmitter Not Accessible"); break;
         case 30:    fprintf( stderr, "WARNING(30): Extended Range Transmitter Not Attached"); break;
@@ -406,11 +473,11 @@ int PointATC3DG::check_bird_errors( void )
         case 33:    fprintf( stderr, "WARNING(33): Slave Configuration"); break;
         case 34:    fprintf( stderr, "WARNING(34): Watch Dog Timer"); break;
         case 35:    fprintf( stderr, "WARNING(35): Over Temperature"); break;
-        default:    fprintf( stderr, "WARNING(%d): Unknown Error Code", datain[0]);
+		default:    std::cerr << "WARNING(" << datain[0] << ": Unknown Error Code"; break;
     }
-    fprintf( stderr, "\n");
+	std::cerr << std::endl;
 
-    if( fatal ) {
+    if (fatal) {
         isOk = false;
         exit( datain[0]);
     }
